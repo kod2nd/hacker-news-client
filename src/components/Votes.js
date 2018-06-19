2e29da881817132cc283ba21d3073359a7550dbb
kod2nd <colinongengkiat@gmail.com>
2018-06-19 15:15:59 +0800

    Cleaned up code

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,42 +1,3 @@
-// import React, { Component } from 'react';
-
-// class Votes extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             votes: props.votes
-//         }
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 {/* <div className="vote-container">
-//                     <div>
-//                         {"Votes: " + this.state.votes}
-//                     </div>
-//                     <button onClick={() => {
-//                         return this.increaseVoteValue()
-//                     }}>+</button>
-//                     <button onClick={() => {return this.decreaseVoteValue()}}>-</button>
-//                 </div> */}
-//             </div>
-//         );
-//     }
-
-//     // increaseVoteValue() {
-//     //     this.setState({
-//     //         votes: this.state.votes + 1
-//     //     })
-//     // }
-
-//     // decreaseVoteValue() {
-//     //     this.setState({
-//     //         votes: this.state.votes - 1
-//     //     })
-//     // }
-// }
-
 import React from 'react';
 
 const Votes = (props) => (
@@ -52,7 +13,5 @@ const Votes = (props) => (
         </div>
     </div>
 );
-
-
 
 export default Votes;
